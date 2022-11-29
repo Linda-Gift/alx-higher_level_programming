106af70162834df18a6cee8e86555427429946a2
engrlindagift <lindagift0812@gmail.com>
2022-11-28 17:11:37 -0800

    Task 6

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-Str1 = "Holberton"
-Str2 =  "School"
-Str1 = Str1 + ' ' + Str2
-print(f"welcome to {Str1}!")
+str1 = "Holberton"
+str2 = "School"
+str1 += " " + str2
+print("Welcome to {}!".format(str1)
