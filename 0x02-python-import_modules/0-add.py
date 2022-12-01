f7bfd07d638e286cf7e17f2f5ca8c0cea7f19673
engrlindagift <lindagift0812@gmail.com>
2022-12-01 08:51:31 -0800

    Task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python
-def add(a, b):
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
+
     a = 1
     b = 2
-    add = a + b
-    print("<a value>{} + <b value>{} = <add(a, b) value>{}".format(a, b, add))
-    return add
+
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
