3b4f135520556f1df6981cd5602d7ad1bee2f02d
engrlindagift <lindagift0812@gmail.com>
2022-12-01 08:38:34 -0800

    Task 1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+calculator_1
+
+def add(a, b):
+    """My addition function
+
+    a = 10
+    b = 5
+    result = a + b
+    print("{} + {} = {}".format(a, b, result))
+    
+    """
+
+    return (a + b)
+
+
+def sub(a, b):
+    """My subtraction function
+
+    a = 10
+    b = 5
+    result = a - b
+    print("{} - {} = {}".format(a, b, result))
+    
+    """
+
+    return (a - b)
+
+
+def mul(a, b):
+    """My multiplication function
+    
+    a = 10
+    b = 5
+    result = a * b
+    print("{} * {} = {}".format(a, b, result))
+    
+    """
+
+    return (a * b)
+
+
+def div(a, b):
+    """My division function
+
+    a = 10
+    b = 5
+    result = a / b
+    print("{} / {} = {}".format(a, b, result))
+    
+    """
+
+    return (a / b)
+
+
