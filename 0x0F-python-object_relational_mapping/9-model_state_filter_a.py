db1288eccb4eb81f1e2e2b6515d300b721dd7a26
engrlindagift <lindagift0812@gmail.com>
2023-03-23 21:09:16 +0000

    Task 9

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
-"""Script that lists all objects that contain the a letter from the database
-"""
-import sys
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import (create_engine)
+""" a script that prints the State object with the name passed as argument
+    from the database hbtn_0e_6_usa. Results must display the states.id
+    """
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    import sys
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
 
-    for states in session.query(State).filter(State.name.contains('%a')):
-        print("{}: {}".format(states.id, states.name))
+
+    if __name__ == "__main__":
+        engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                               .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+        # Create all the tables associated with the Base metadata
+        Base.metadata.create_all(engine)
+
+        # Create a new Session instance bound to the engine we created
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        # Query the State object from the database
+        state = session.query(State).filter(State.name == sys.argv[4]).first()
+
+        if state:
+            print(state.id)
+        else:
+            print("Not found")
+
+        session.close()
