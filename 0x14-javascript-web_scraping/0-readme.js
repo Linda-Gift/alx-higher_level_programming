f5c052b791130788acc63abcfb874811ee126bc0
engrlindagift <lindagift0812@gmail.com>
2023-05-04 15:22:21 +0000

    Task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
-filePath = process.argv[2];
-fs.readfile(filePath, (err, data) ==> {
+const filePath = process.argv[2];
+fs.readFile(filePath, 'utf-8',  (err, data) => {
   if (err) {
-    console.log(err);
+    console.error(err);
+    return;
   }
   else {
     console.log(data);
