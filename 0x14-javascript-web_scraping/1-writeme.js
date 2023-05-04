7839be50c35737ecb79029ebb248696bf615f907
engrlindagift <lindagift0812@gmail.com>
2023-05-04 15:55:59 +0000

    Task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], error => {
-  if (error) console.log(error);
+const filePath = (process.argv[2];
+const data = process.argv[3];
+fs.writeFile(filePath, data (err) => {
+  if (err) {
+    console.log(err);
+  }
 });
